b94c1ad66ff3ea9c7416dcd4612695f5254570c3
cy <2449471714@qq.com>
2022-10-25 16:57:14 +0800

    01_注册登录

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,29 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import User from '../views/User.vue'
+import Index from '../views/Index.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/user',
+    name: 'User',
+    component: User
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/',
+    name: 'Index',
+    component: Index
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to,form,next)=>{
+  next();
 })
 
 export default router
---
+++ src/views/User.vue
@@ -0,0 +1,65 @@
+<template>
+  <!-- 主页 -->
+  <div class="home">
+    <div class="homeContainer">
+      <!-- 主页标题 -->
+      <div class="header">
+        <!-- 标题 -->
+        <div class="homeTitle">Easy Configuration</div>
+        <!-- 登录按钮 -->
+        <div class="logBtnC">
+        <el-button type="primary" size="small">登录/注册</el-button>
+        </div>
+      </div>
+
+      <!-- 主页主体 -->
+      <div> 我是主体 </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      
+    };
+  },
+  created(){
+    //验证cookie
+    // let cookie = 
+  }
+  
+};
+</script>
+
+<style scoped>
+.homeContainer{
+  height: 100vh;
+  width:100vw;
+}
+.header {
+  height: 8vh;
+  background-color: rgb(41,42,43);
+  display: flex;
+  justify-content: center;
+  position: relative;
+}
+.homeTitle {
+  height: 8vh;
+  text-align: center;
+  line-height: 8vh;
+  color: white;
+  font-weight: bolder;
+  font-size: 2rem;
+}
+.logBtnC{
+  position: absolute;
+  height: 8vh;
+  right: 1vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
