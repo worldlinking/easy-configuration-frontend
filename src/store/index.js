bb4de8d1dfbbbb05a5b12448e1e3d6b36d071ac5
cy <2449471714@qq.com>
2022-11-14 15:46:10 +0800

    11_视觉模型部署

--- src/store/index.js
+++ src/store/index.js
@@ -16,12 +16,15 @@ const options = {
     currentWeight: [],
     predictImgSrc:
       "https://img.tukuppt.com/png_preview/00/04/81/SYZxWQlAr9.jpg!/fw/780",
+    predictZipSrc: "暂无结果",
+    predictStatus: 0,
   },
   actions: {},
   mutations: {
     initModelParams(state, { modelIndex, type }) {
       state.modelIndex = modelIndex;
       state.type = type;
+      console.log(state.modelIndex);
     },
     async getStandModel(state) {
       //根据模型种类获取所有的标准模型
@@ -46,32 +49,74 @@ const options = {
       });
       state.currentWeight = smws;
     },
-    async predict(state, {current_file,weight_id,standModel_id,cp}) {
-      //上传文件进行预测
-      if (!current_file) {
-        cp.$message({
-            type:"error",
-            message:"请先上传文件！"
-        });
-        return;
-      }
+    async predict(
+      state,
+      { current_file, weight_id, standModel_id, cp, predictType }
+    ) {
+      state.predictStatus = 1;
+      if (predictType == "image") {
+        //上传文件进行预测
+        if (!current_file) {
+          cp.$message({
+            type: "error",
+            message: "请先上传文件！",
+          });
+          return;
+        }
 
-      let postUrl = `${ip}/useStandModelWeightImage`;
-      const formData = new FormData();
-      
-      formData.append("weight_id", parseInt(weight_id));
-      formData.append("standModel_id", parseInt(standModel_id));
-      formData.append("predict_file", current_file);
-      formData.append("user_id", state.user_id);
-      let res = await axios.post(postUrl, formData);
-      if (res.data.code == 200) {
-        this.$message({
-          type: "success",
-          message: "预测成功!",
-        });
+        let postUrl = `${ip}/useStandModelWeightImage`;
+        const formData = new FormData();
 
-        state.predictImgSrc = nginxIp + "\\" + res.data.data;
+        formData.append("weight_id", parseInt(weight_id));
+        formData.append("standModel_id", parseInt(standModel_id));
+        formData.append("predict_file", current_file);
+        formData.append("user_id", state.user_id);
+        let res = await axios.post(postUrl, formData);
+        if (res.data.code == 200) {
+          cp.$message({
+            type: "success",
+            message: "预测成功!",
+          });
+
+          state.predictImgSrc = nginxIp + "\\" + res.data.data;
+          state.predictStatus = 2;
+        } else {
+          state.predictStatus = 3;
+        }
+      } else if (predictType == "zip") {
+        if (!current_file) {
+          cp.$message({
+            type: "error",
+            message: "请先上传文件！",
+          });
+          return;
+        }
+
+        let postUrl = `${ip}/useStandModelWeightZip`;
+        const formData = new FormData();
+        formData.append("weight_id", parseInt(weight_id));
+        formData.append("standModel_id", parseInt(standModel_id));
+        formData.append("predict_zip", current_file);
+        formData.append("user_id", state.user_id);
+        let res = await axios.post(postUrl, formData);
+        if (res.data.code == 200) {
+          cp.$message({
+            type: "success",
+            message: "预测成功!",
+          });
+
+          state.predictZipSrc = nginxIp + "\\" + res.data.data;
+          state.predictStatus = 2;
+        } else {
+          state.predictStatus = 3;
+        }
       }
+    },
+    reUpload(state) {
+      state.predictImgSrc =
+        "https://img.tukuppt.com/png_preview/00/04/81/SYZxWQlAr9.jpg!/fw/780";
+      state.predictZipSrc = "暂无结果";
+      state.predictStatus = 0;
     },
   },
 };
--- src/views/User/UsePublicModel.vue
+++ src/views/User/UsePublicModel.vue
@@ -2,20 +2,36 @@
   <div class="useMyModel">
     <el-form ref="form" :model="form" label-width="500">
       <el-form-item label="标准模型">
-        <el-select v-model="form.standModel" placeholder="请选择标准模型" @change="smChange">
-          <el-option v-for="sm,index in currentStandModel" :key="index" :label="sm.name" :value="sm.id"></el-option>
+        <el-select
+          v-model="form.standModel"
+          placeholder="请选择标准模型"
+          @change="smChange"
+        >
+          <el-option
+            v-for="(sm, index) in currentStandModel"
+            :key="index"
+            :label="sm.name"
+            :value="sm.id"
+          ></el-option>
         </el-select>
       </el-form-item>
 
       <el-form-item label="训练权重">
         <el-select v-model="form.standModelWeight" placeholder="请选择训练权重">
-          <el-option v-for="sm,index in currentWeight" :key="index" :label="sm.fields.weight_path" :value="sm.pk"></el-option>
+          <el-option
+            v-for="(sm, index) in currentWeight"
+            :key="index"
+            :label="
+              sm.fields.weight_path + '  ---' + sm.fields.dataset + '数据集'
+            "
+            :value="sm.pk"
+          ></el-option>
         </el-select>
       </el-form-item>
 
       <el-row>
         <el-col :span="12">
-          <el-form-item label="上传待预测文件">
+          <el-form-item label="上传待预测样本">
             <el-upload
               class="avatar-uploader"
               :show-file-list="false"
@@ -23,25 +39,36 @@
               :before-upload="beforeUpload"
             >
               <img
-                v-if="imageUrl"
+                v-if="predictType == 'image'"
                 :src="imageUrl"
                 class="avatar"
                 width="500"
                 height="300"
               />
-              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+              <div v-if="predictType == 'zip'">{{ currentFile.name }}</div>
+              <i
+                v-if="!predictType"
+                class="el-icon-plus avatar-uploader-icon"
+              ></i>
             </el-upload>
           </el-form-item>
         </el-col>
         <el-col :span="12">
           <el-form-item label="预测结果">
-            <img :src="predictImgSrc" width="500" height="300" />
+            <img
+              :src="predictImgLink"
+              width="500"
+              height="300"
+              v-if="predictType == 'image' && predictImgLink != ''"
+              @error="imgOnError"
+            />
+            <div v-show="predictType == 'zip'">{{ predictZipSrc }}</div>
           </el-form-item>
         </el-col>
       </el-row>
       <el-form-item label="预测与下载">
         <el-button type="primary" @click="predictS">预测</el-button>
-        <el-button type="downPredict">下载</el-button>
+        <el-button @click="downPredict">下载</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -50,8 +77,8 @@
 <script>
 import axios from "axios";
 import config from "../../assets/configs/config";
-import {mapState,mapMutations} from 'vuex'
-let { ip,nginxIp } = config;
+import { mapState, mapMutations } from "vuex";
+let { ip, nginxIp } = config;
 export default {
   name: "WorkspaceJsonUseMyModel",
 
@@ -64,21 +91,56 @@ export default {
       imageUrl: "",
       uploadImgBase64: "",
       currentFile: null,
+      predictType: "",
+      error: false,
     };
   },
-  computed:{
-    ...mapState(["currentStandModel","currentWeight","predictImgSrc"]),
+  computed: {
+    ...mapState([
+      "currentStandModel",
+      "currentWeight",
+      "predictImgSrc",
+      "predictZipSrc",
+      "predictStatus",
+    ]),
+    predictImgLink() {
+      if (this.predictStatus != 2) return this.predictImgSrc;
+      if (!this.error) {
+        return this.predictImgSrc + ".png";
+      } else {
+        return this.predictImgSrc + ".jpg";
+      }
+    },
   },
-  async created(){
+  async created() {
     await this.getStandModel();
   },
   mounted() {},
 
   methods: {
-    ...mapMutations(["getStandModel","initModelParams","updateCurrentWeight","predict"]),
+    ...mapMutations([
+      "getStandModel",
+      "initModelParams",
+      "updateCurrentWeight",
+      "predict",
+      "reUpload",
+    ]),
     beforeUpload(file) {
-      this.toBase64(file);
-      this.currentFile = file;
+      if (file.type == "image/jpeg" || file.type == "image/png") {
+        this.predictType = "image";
+        this.toBase64(file);
+        this.currentFile = file;
+        this.reUpload();
+      } else if (file.type == "application/x-zip-compressed") {
+        this.predictType = "zip";
+        this.currentFile = file;
+        this.reUpload();
+      } else {
+        this.$message({
+          type: "error",
+          message: "只接受.png,.jpg或者zip文件",
+        });
+      }
       return false;
     },
     toBase64(file) {
@@ -92,14 +154,59 @@ export default {
       }
     },
     async predictS() {
-      this.predict({current_file:this.currentFile,weight_id:this.form.standModelWeight,standModel_id:this.form.standModel,cp:this});
+      if (this.predictStatus == 1) {
+        this.$message({
+          type: "error",
+          message: "预测中,请勿重复点击!",
+        });
+        return;
+      } else if (this.predictStatus == 2) {
+        this.$message({
+          type: "warning",
+          message: "已预测完成，请勿重复预测!",
+        });
+        return;
+      }
+      this.predict({
+        current_file: this.currentFile,
+        weight_id: this.form.standModelWeight,
+        standModel_id: this.form.standModel,
+        cp: this,
+        predictType: this.predictType,
+      });
     },
-    downPredict(){
-
+    downPredict() {
+      if (this.predictType == "image") {
+        let img = new Image();
+        img.setAttribute("crossOrigin", "Anonymous");
+        img.onload = function () {
+          let canvas = document.createElement("canvas");
+          let context = canvas.getContext("2d");
+          canvas.width = img.width;
+          canvas.height = img.height;
+          context.drawImage(img, 0, 0, img.width, img.height);
+          let url = canvas.toDataURL("images/png");
+          let a = document.createElement("a");
+          let event = new MouseEvent("click");
+          a.download = "predict.png" || "default.png";
+          a.href = url;
+          a.dispatchEvent(event);
+        };
+        img.src = this.predictImgLink + "?v=" + Date.now();
+      } else if (this.predictType == "zip") {
+        let a = document.createElement("a");
+        let event = new MouseEvent("click");
+        a.download = "predict.zip";
+        a.href = this.predictZipSrc.replace("\\", "/");
+        a.dispatchEvent(event);
+      }
     },
-    smChange(newSm){
+    smChange(newSm) {
       this.updateCurrentWeight(newSm);
-    }
+    },
+    imgOnError() {
+      this.error = true;
+    },
   },
   watch: {
     uploadImgBase64(newBase) {
